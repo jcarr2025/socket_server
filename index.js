664ab4257a5bb9cd38e382c5bb4b06767593ed91
jcarreno <jcarreno@payall.com.ve>
2026-02-26 16:43:51 -0400

    🐛 Corregir la funcion que genera los numeros aleatorios

--- index.js
+++ index.js
@@ -9,16 +9,15 @@ console.log("Servidor emitiendo números aleatorios...");
 io.on("connection", (socket) => {
   console.log("Cliente conectado:", socket.id);
 
-  // Creamos un intervalo que se ejecuta cada segundo
-  const intervalo = 
-  (() => {
-    const numeroAleatorio = Math.floor(Math.random() * 100); // Número entre 0 y 99
+  // CORRECCIÓN: Agregamos setInterval antes del paréntesis
+  const intervalo = setInterval(() => {
+    const numeroAleatorio = Math.floor(Math.random() * 100);
     
-    // Enviamos el dato al cliente con el evento 'datos_servidor'
+    console.log(`Enviando a ${socket.id}: ${numeroAleatorio}`); // Para que veas actividad en la terminal
+    
     socket.emit("datos_servidor", { valor: numeroAleatorio });
   }, 1000);
 
-  // Importante: Limpiar el intervalo cuando el cliente se desconecte
   socket.on("disconnect", () => {
     clearInterval(intervalo);
     console.log("Cliente desconectado, deteniendo emisión.");
